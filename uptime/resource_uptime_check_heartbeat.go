944db471011b32d3134db1a2b9897408cbec382d
Liam Kinne <lkinne@integral.com.au>
2022-01-19 18:04:15 +1000

    fmt and strings not needed

--- uptime/resource_uptime_check_heartbeat.go
+++ uptime/resource_uptime_check_heartbeat.go
@@ -1,12 +1,8 @@
 package uptime
 
 import (
-	"fmt"
-
 	uptime "github.com/uptime-com/rest-api-clients/golang/uptime"
 	"github.com/hashicorp/terraform/helper/schema"
-
-	"strings"
 )
 
 func resourceUptimeCheckHeartbeat() *schema.Resource {
